46d7ea28a5fbf1554a4b9a1febfa14eea75cc60e
Шевченко Владислав <129328232+guccivlad@users.noreply.github.com>
2023-04-23 15:57:03 +0300

    Add files via upload

---
+++ telebot.py
@@ -0,0 +1,233 @@
+from __future__ import print_function
+
+import logging
+import os.path
+import io
+import google.auth
+import pickle
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from dotenv import dotenv_values
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
+
+config = dotenv_values(".env")
+allowed_extensions = ['pdf', 'jpg']
+folder_id = '1_dMZ7eMrTfECHJ3W6N4owefXNqYaGpmM'
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+def Create_Service(client_secret_file, api_name, api_version, *scopes):
+    print(client_secret_file, api_name, api_version, scopes, sep='-')
+    CLIENT_SECRET_FILE = client_secret_file
+    API_SERVICE_NAME = api_name
+    API_VERSION = api_version
+    SCOPES = [scope for scope in scopes[0]]
+    print(SCOPES)
+
+    cred = None
+
+    pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}.pickle'
+    # print(pickle_file)
+
+    if os.path.exists(pickle_file):
+        with open(pickle_file, 'rb') as token:
+            cred = pickle.load(token)
+
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            cred = flow.run_local_server()
+
+        with open(pickle_file, 'wb') as token:
+            pickle.dump(cred, token)
+
+    try:
+        service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
+        print(API_SERVICE_NAME, 'service created successfully')
+        return service
+    except Exception as e:
+        print('Unable to connect.')
+        print(e)
+        return None
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+async def show_files(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # If modifying these scopes, delete the file token.json.
+    SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('drive', 'v3', credentials=creds)
+
+        # Call the Drive v3 API
+        results = service.files().list(
+            pageSize=10, fields="nextPageToken, files(id, name)").execute()
+        items = results.get('files', [])
+
+        if not items:
+            print('No files found.')
+            return
+        print('Files:')
+        for item in items:
+            extension = item['name'].split('.')
+            if(len(extension) > 1):
+                if(extension[1] in allowed_extensions):
+                    await context.bot.send_message(chat_id=update.effective_chat.id, text=item.get('name'))
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+    except HttpError as error:
+        # TODO(developer) - Handle errors from drive API.
+        print(f'An error occurred: {error}')
+
+def download_file(real_file_id):
+    """Downloads a file
+    Args:
+        real_file_id: ID of the file to download
+    Returns : IO object with location.
+    """
+
+    CLIENT_SECRET_FILE = 'credentials.json'
+    API_NAME = 'drive'
+    API_VERSION = 'v3'
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+
+    file_name = 'book.pdf'
+
+    try:
+        service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+        file_id = real_file_id
+
+        # pylint: disable=maybe-no-member
+        request = service.files().get_media(fileId=file_id)
+        file = io.BytesIO()
+        downloader = MediaIoBaseDownload(file, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print(F'Download {int(status.progress() * 100)}.')
+        
+        name = 'convex.pdf'
+
+        file.seek(0)
+
+        with open(os.path.join('./server', file_name), 'wb') as f:
+            f.write(file.read())
+            os.rename(os.path.join('./server', file_name), os.path.join('./server', name))
+            f.close()
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        file = None
+
+    return name
+
+async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    file_id = text.split(' ')[1]
+    file_name = download_file(file_id)
+    await context.bot.send_document(chat_id=update.effective_chat.id, document=open(f'./server/{file_name}', 'rb'))
+
+async def search(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    file_name = text[8 :]
+    print(file_name)
+    files = search_file()
+    if(file_name in files):
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Yep, the file exists!")
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="The file does not exist.")
+
+
+def search_file():
+    """Search file in drive location
+
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+
+    CLIENT_SECRET_FILE = 'credentials.json'
+    API_NAME = 'drive'
+    API_VERSION = 'v3'
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+
+    try:
+        # create drive api client
+        service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+        files = []
+        files_dict = {}
+        page_token = None
+        query = f"parents = '{folder_id}'"
+        
+        while True:
+            # pylint: disable=maybe-no-member
+            response = service.files().list(q=query,).execute()
+            for file in response.get('files', []):
+                # Process change
+                files_dict[file.get('name')] = file.get('id')
+            files.extend(response.get('files', []))
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        files = None
+
+    return files_dict
+
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(config['TOKEN']).build()
+    
+    start_handler = CommandHandler('start', start)
+    hello_handler = CommandHandler('hello', hello)
+    show_files_handler = CommandHandler('show_files', show_files)
+    download_handler = CommandHandler('download', download)
+    search_handler = CommandHandler('search', search)
+
+    application.add_handler(start_handler)
+    application.add_handler(hello_handler)
+    application.add_handler(show_files_handler)
+    application.add_handler(download_handler)
+    application.add_handler(search_handler)
+    
+    application.run_polling()
